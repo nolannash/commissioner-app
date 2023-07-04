0c728f9a8c9ce101c1dfa421c925f4e5ae4e247f
nolan nash <n0lan.n4sh@gmail.com>
2023-07-03 18:01:47 -0700

    client -- npm install || server -- pipenv install

--- server/config.py
+++ server/config.py
@@ -1,31 +1,31 @@
-# Standard library imports
-
-# Remote library imports
 from flask import Flask
-from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from sqlalchemy import MetaData
+import os
 
-# Local imports
-
-# Instantiate app, set attributes
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+app.secret_key = os.environ.get('SECRET_KEY', 'dev')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-# Define metadata, instantiate db
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
+
 db = SQLAlchemy(metadata=metadata)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db,render_as_batch=True)
 db.init_app(app)
 
-# Instantiate REST API
+render_as_batch=True
+
+bcrypt = Bcrypt(app)
+
 api = Api(app)
 
-# Instantiate CORS
 CORS(app)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
-
-from config import db
-
-# Models go here!
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.ext.associationproxy import association_proxy
+from config import db, bcrypt
+import re
