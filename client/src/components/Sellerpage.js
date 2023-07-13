18efebc559ca557a9c7f8822dd96e0685c0ccf61
nolan nash <n0lan.n4sh@gmail.com>
2023-07-12 17:57:10 -0700

    update seller homepage view

--- client/src/components/Sellerpage.js
+++ client/src/components/Sellerpage.js
@@ -4,59 +4,64 @@ import {
   Toolbar,
   Typography,
   Button,
-  Menu,
-  MenuItem,
   Box,
-  Divider,
-  Tooltip,
-  Avatar,
-  ListItemIcon,
+  Tab,
+  Tabs,
+  Popover,
 } from '@mui/material';
-import { Search as SearchIcon, ViewList, Receipt, AccountBox } from '@mui/icons-material';
+import {
+  Search as SearchIcon,
+  ViewList,
+  Receipt,
+  AccountBox,
+  DeleteSharp,
+} from '@mui/icons-material';
 import { AuthContext } from '../contexts/AuthContext';
 import SellerItems from './SellerItemsPage';
-import {useHistory,} from 'react-router-dom';
 
 const SellerPage = () => {
   const { user, logout } = useContext(AuthContext);
-  const history = useHistory()
+  const [activeSection, setActiveSection] = useState(0);
+  const [deletePopoverOpen, setDeletePopoverOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
-  const [activeSection, setActiveSection] = useState('');
 
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
+  const handleTabChange = (event, newValue) => {
+    setActiveSection(newValue);
+  };
+
+  const handleDeleteProfileClick = (event) => {
     setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
+    setDeletePopoverOpen(true);
   };
 
-  const handleSectionClick = (section) => {
-    setActiveSection(section);
-    handleClose();
+  const handleDeleteProfileConfirm = async () => {
+    try {
+      // Perform the profile deletion API request
+      // ...
+
+      // After successful deletion
+      setDeletePopoverOpen(false);
+      logout();
+    } catch (error) {
+      console.error('Profile deletion error:', error.message);
+      // Handle error if necessary
+    }
+  };
+
+  const handleDeletePopoverClose = () => {
+    setDeletePopoverOpen(false);
   };
 
   const renderSection = () => {
     switch (activeSection) {
-      case 'accountInfo':
-        return <AccountInfo />;
-      case 'items':
+      case 0:
         return <SellerItems />;
-      case 'orders':
+      case 1:
         return <Orders />;
+      case 2:
+        return <AccountInfo handleDeleteProfileClick={handleDeleteProfileClick} />;
       default:
-        return <AccountInfo />;
-    }
-  };
-
-  const handleLogout = async () => {
-    try {
-      history.push('/landing')
-      logout(); 
-
-    } catch (error) {
-      console.error('Logout error:', error.message);
-
+        return <AccountInfo handleDeleteProfileClick={handleDeleteProfileClick} />;
     }
   };
 
@@ -67,90 +72,71 @@ const SellerPage = () => {
           {user.shopname}
         </Typography>
         <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-          <Button variant="contained" onClick={handleLogout} sx={{ ml: 2 }}>
+          <Button variant="contained" onClick={logout} sx={{ ml: 2 }}>
             Logout
           </Button>
-          <Tooltip title="Account settings">
-            <Button
-              onClick={handleClick}
-              size="small"
-              sx={{ ml: 2 }}
-              aria-controls={open ? 'account-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-            >
-              <Avatar sx={{ width: 32, height: 32 }}>
-                <AccountBox />
-              </Avatar>
-            </Button>
-          </Tooltip>
-          <Menu
-            anchorEl={anchorEl}
-            id="account-menu"
-            open={open}
-            onClose={handleClose}
-            onClick={handleClose}
-            PaperProps={{
-              elevation: 0,
-              sx: {
-                overflow: 'visible',
-                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                mt: 1.5,
-                '& .MuiAvatar-root': {
-                  width: 32,
-                  height: 32,
-                  ml: -0.5,
-                  mr: 1,
-                },
-                '&:before': {
-                  content: '""',
-                  display: 'block',
-                  position: 'absolute',
-                  top: 0,
-                  right: 14,
-                  width: 10,
-                  height: 10,
-                  bgcolor: 'background.paper',
-                  transform: 'translateY(-50%) rotate(45deg)',
-                  zIndex: 0,
-                },
-              },
-            }}
-            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-          >
-            <Divider />
-            <MenuItem onClick={() => handleSectionClick('accountInfo')}>
-              <ListItemIcon>
-                <AccountBox fontSize="small" />
-              </ListItemIcon>
-              Account Info
-            </MenuItem>
-            <MenuItem onClick={() => handleSectionClick('items')}>
-              <ListItemIcon>
-                <ViewList fontSize="small" />
-              </ListItemIcon>
-              Items
-            </MenuItem>
-            <MenuItem onClick={() => handleSectionClick('orders')}>
-              <ListItemIcon>
-                <Receipt fontSize="small" />
-              </ListItemIcon>
-              Orders
-            </MenuItem>
-          </Menu>
         </Box>
       </Toolbar>
+      <Tabs
+        value={activeSection}
+        onChange={handleTabChange}
+        centered
+        indicatorColor="primary"
+        textColor="primary"
+        sx={{
+          '& .Mui-selected': {
+            bgcolor: 'primary.contrastText',
+            color: 'primary.main',
+          },
+        }}
+      >
+        <Tab label="Items" icon={<ViewList />} />
+        <Tab label="Orders" icon={<Receipt />} />
+        <Tab label="Profile" icon={<AccountBox />} />
+      </Tabs>
       {renderSection()}
+      <Popover
+        open={deletePopoverOpen}
+        anchorEl={anchorEl}
+        onClose={handleDeletePopoverClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Box sx={{ p: 2 }}>
+          <Typography variant="body1">
+            Are you sure you want to delete your profile?
+          </Typography>
+          <Button variant="contained" color="error" onClick={handleDeleteProfileConfirm}>
+            Click here to confirm
+          </Button>
+        </Box>
+      </Popover>
     </AppBar>
   );
 };
 
-const AccountInfo = () => {
+const AccountInfo = ({ handleDeleteProfileClick }) => {
   return (
     <div>
       <Typography variant="h6">Account Information</Typography>
-      {/* Render account information */}
+      <Button variant="contained" size="medium" color="secondary">
+        Edit Profile
+      </Button>
+      <Button
+        variant="contained"
+        color="error"
+        size="medium"
+        startIcon={<DeleteSharp />}
+        onClick={handleDeleteProfileClick}
+      >
+        Delete Profile
+      </Button>
     </div>
   );
 };
